7f8e81a0e3c9cbf5da4195066d4538a57f7d7c09
myt72 <mtomonaga@gmail.com>
2026-09-15 14:14:47 -0700

    Update StartAll.bat to set new ports for server and client

--- StartAll.bat
+++ StartAll.bat
@@ -1,7 +1,9 @@
 @echo off
 setlocal
-set "BACKEND_PORT=%~1"
-if "%BACKEND_PORT%"=="" set "BACKEND_PORT=3001"
 
-start "MusicStats Server" cmd /k "cd /d %~dp0 && set PORT=%BACKEND_PORT% && npm run server"
-start "MusicStats Client" cmd /k "cd /d %~dp0src\client && set HOST=0.0.0.0 && set REACT_APP_API_PORT=%BACKEND_PORT% && npm start"
+cd /d H:\Development\MusicStats
+
+start "MusicStats Server" cmd /k "set PORT=3002&& npm run server"
+start "MusicStats Client" cmd /k "cd src\client&& set HOST=0.0.0.0&& set PORT=3003&& set REACT_APP_API_PORT=3002&& npm start"
+
+endlocal
